0439439f1f12bd70d64c44e40b73559515502c79
Arnav Hosamani <arnavbh13@gmail.com>
2025-07-23 01:28:48 +0530

    comfig changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 
-<React.StrictMode>
-  <HashRouter>
-    <App />
-  </HashRouter>
-</React.StrictMode>
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+root.render(
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+)
