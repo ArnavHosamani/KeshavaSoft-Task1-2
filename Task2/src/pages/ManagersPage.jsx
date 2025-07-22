defb8ff2af8a06885a913cff4dce890072eaffc7
Arnav Hosamani <151857200+ArnavHosamani@users.noreply.github.com>
2025-07-23 01:11:04 +0530

    Delete Task2 directory

--- Task2/src/pages/ManagersPage.jsx
+++
@@ -1,7 +0,0 @@
-import ManagerList from "../components/ManagerList";
-
-function ManagersPage({ employees }) {
-  return <ManagerList employees={employees} />;
-}
-
-export default ManagersPage;
--- Task2/src/pages/RegisterPage.jsx
+++
@@ -1,7 +0,0 @@
-import EmployeeForm from "../components/EmployeeForm";
-
-function RegisterPage({ employees, setEmployees }) {
-  return <EmployeeForm employees={employees} setEmployees={setEmployees} />;
-}
-
-export default RegisterPage;
